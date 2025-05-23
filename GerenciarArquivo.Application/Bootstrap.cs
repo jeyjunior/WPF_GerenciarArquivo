33a2be0ed2da674b04286b77b6fa109a9afb25e7
jeyjunior <jeyjuniordg@gmail.com>
2025-05-22 22:01:35 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciarArquivo.Application/Bootstrap.cs
@@ -0,0 +1,38 @@
+﻿using JJ.NET.CrossData.Services;
+using JJ.NET.Data;
+using JJ.NET.Data.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciarArquivo.Application
+{
+    public static class Bootstrap
+    {
+        public static System.IServiceProvider ServiceProvider { get; private set; }
+
+        public static async Task IniciarAsync()
+        {
+            var config = new ConfiguracaoBancoDadosService();
+            var result = await config.IniciarConfiguracaoAsync(JJ.NET.CrossData.Enumerador.Conexao.SQLite);
+
+            if (!result.Sucesso)
+                throw new Exception(result.Erros.First());
+            
+            var services = new ServiceCollection();
+
+            // Registro das dependências
+            services.AddSingleton<IUnitOfWork>(_ => new UnitOfWork(result.DbConnection));
+
+            //services.AddSingleton<IArquivoRepository, ArquivoRepository>();
+            //services.AddSingleton<IParametroRepository, ParametroRepository>();
+            //services.AddSingleton<IArquivoAppService, ArquivoAppService>();
+            //services.AddSingleton<IParametroAppService, ParametroAppService>();
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
---
+++ GerenciarArquivo/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using GerenciarArquivo.Application;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GerenciarArquivo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            try
+            {
+                await Bootstrap.IniciarAsync();
+
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Falha ao iniciar aplicação: {ex.Message}",
+                               "Erro",
+                               MessageBoxButton.OK,
+                               MessageBoxImage.Error);
+                Shutdown();
+            }
+        }
+    }
+}
